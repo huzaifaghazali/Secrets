61ef5447502efa2aac28ca8db5ffda32c3058d71
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2022-01-18 15:50:13 +0500

    Add hash method

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ require('dotenv').config();
 const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 
@@ -23,9 +21,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-
-// for encryption encrypt only password field
-userSchema.plugin(encrypt, { secret: process.env.SECRET,  encryptedFields: ['password'] });
 
 // create the model
 const User = new mongoose.model("User", userSchema);
@@ -48,7 +43,7 @@ app.post("/register", function(req, res){
   // create a new user when user register
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -61,7 +56,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
